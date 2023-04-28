5d3a24d6e322c94a47740506af06257fe00a107c
Aaryaman Pol <Aaryamaanpol15@gmail.com>
2023-04-28 13:49:27 -0500

    Clean tree

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
         EntityManager entityManager = emf.createEntityManager();
         try {
